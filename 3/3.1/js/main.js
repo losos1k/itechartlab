27e46064779573e5daced5393adf87e0d487ae39
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-21 19:04:12 +0300

    refactor

--- 3/3.1/js/main.js
+++ 3/3.1/js/main.js
@@ -9,14 +9,21 @@ var resultFromNow = document.getElementById("resultFromNow");
 submitBtn.addEventListener("click", btnClick);
 fromNow.addEventListener("click", btnFromNow);
 
+var inputTemplateValidate = function () {
+    return (INPUT_TEMPLATE.indexOf('D') != -1) && (INPUT_TEMPLATE.indexOf('M') != -1) && (INPUT_TEMPLATE.indexOf('Y') != -1);
+}
+var outputTemplateValidate = function () {
+    return (OUTPUT_TEMPLATE.indexOf('D') != -1) &&
+        ((OUTPUT_TEMPLATE.indexOf('M') != -1) || (OUTPUT_TEMPLATE.indexOf('month') != -1)) &&
+        (OUTPUT_TEMPLATE.indexOf('Y') != -1);
+}
+
 function btnClick(e) {
     e.preventDefault();
     insDate = document.getElementById("insDate").value;
     INPUT_TEMPLATE = document.getElementById("INPUT_TEMPLATE").value;
     OUTPUT_TEMPLATE = document.getElementById("OUTPUT_TEMPLATE").value;
-    if ((INPUT_TEMPLATE.indexOf('D') != -1) && (INPUT_TEMPLATE.indexOf('M') != -1) && (INPUT_TEMPLATE.indexOf('Y') != -1) &&
-        (OUTPUT_TEMPLATE.indexOf('D') != -1) && ((OUTPUT_TEMPLATE.indexOf('M') != -1) || (OUTPUT_TEMPLATE.indexOf('month') != -1)) &&
-        (OUTPUT_TEMPLATE.indexOf('Y') != -1)) {
+    if (inputTemplateValidate() && outputTemplateValidate()) {
         resultDate.innerHTML = mod.format(insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE);
     }
     else {
@@ -29,7 +36,7 @@ function btnFromNow(e) {
     e.preventDefault();
     insDate = document.getElementById("insDate").value;
     INPUT_TEMPLATE = document.getElementById("INPUT_TEMPLATE").value;
-    if ((INPUT_TEMPLATE.indexOf('D') != -1) && (INPUT_TEMPLATE.indexOf('M') != -1) && (INPUT_TEMPLATE.indexOf('Y') != -1)) {
+    if (inputTemplateValidate()) {
         resultFromNow.innerHTML = mod.fromNow(insDate, INPUT_TEMPLATE);
     }
     else {
