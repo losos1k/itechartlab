c54aae4db602599e3051e2beb6d961f817f27f6e
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-21 18:33:29 +0300

    refactor

--- 3/3.1/js/dateFormat.js
+++ 3/3.1/js/dateFormat.js
@@ -1,82 +1,95 @@
-mod = (function() {
+mod = (function () {
 
     var DATE, year, month, day, date;
 
-    var msInYear = 365*3600*24*1000;
+    var msInYear = 365 * 3600 * 24 * 1000;
 
-    var months = ['January', 
-            'February', 
-            'March', 
-            'April', 
-            'May', 
-            'June', 
-            'July', 
-            'August', 
-            'September', 
-            'October',
-            'November',
-            'December'];
+    var months = ['January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'];
 
     var getDate = function (regEx) {
 
-            return +insDate.substr(INPUT_TEMPLATE.search(regEx), INPUT_TEMPLATE.match(regEx).length);
+        return +insDate.substr(INPUT_TEMPLATE.search(regEx), INPUT_TEMPLATE.match(regEx).length);
+    }
 
-        }
+    var setOutputDate = function (monthStyle) {
 
-    var format = function(insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE) {
-            
-            if (typeof(insDate) === 'number'){
+        return OUTPUT_TEMPLATE.replace(/D+/, day).replace(/M+|month/, monthStyle).replace(/Y+/, year);
+    }
+
+    var insDateValidate = function () {
+        return ((/\d{2}/.test(day)) && (/\d{2}/.test(month)) && (/\d{4}/.test(year)));
+    }
+
+    var format = function (insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE) {
+
+        if (typeof (insDate) === 'number') {
             DATE = new Date(insDate);
 
             year = DATE.getFullYear();
-            month = DATE.getMonth()+1;
+            month = DATE.getMonth() + 1;
             day = DATE.getDate();
 
-            date = OUTPUT_TEMPLATE.replace(/D+/, day).replace(/M+/, month).replace(/Y+/, year);   
+            date = setOutputDate(month);
 
-        } else{
+        } else {
             day = getDate(/D/g);
-            day < 10 ? day = '0' + day : day; 
+            day < 10 ? day = '0' + day : day;
 
-            month = getDate(/M/g);        
-            month < 10 ? month = '0' + month : month; 
+            month = getDate(/M/g);
+            month < 10 ? month = '0' + month : month;
 
             year = getDate(/Y/g);
-            
-            if ((/\d{2}/.test(day)) && (/\d{2}/.test(month)) && (/\d{4}/.test(year))) {
-                
-                date = OUTPUT_TEMPLATE.replace(/D+/, day).replace(/M+/, month).replace(/Y+/, year);      
-                if (/month/.test(OUTPUT_TEMPLATE)){
-                    date = OUTPUT_TEMPLATE.replace(/D+/, day).replace(/month/, months[month-1]).replace(/Y+/, year); 
-                    }
+
+            if (insDateValidate()) {
+                date = setOutputDate(month);
+                if (/month/.test(OUTPUT_TEMPLATE)) {
+                    date = setOutputDate(months[month - 1]);
                 }
-            
+                return date;
             }
-            
-        return date; 
+            else {
+                alert('Inputed date template is incorrect!');
+                resultDate.style.display = "none";
+            }
+        }
+
     }
 
-    var fromNow = function(insDate, INPUT_TEMPLATE) {
+    var fromNow = function (insDate, INPUT_TEMPLATE) {
         day = getDate(/D/g);
+        month = getDate(/M/g);
+        year = getDate(/Y/g);
 
-        month = getDate(/M/g);        
+        if (insDateValidate()) {
+            DATE = new Date(year, (month - 1), day);
+            var now = new Date();
 
-        year = getDate(/Y/g); 
-        
-        DATE = new Date(year, (month-1), day);  
-        
-        var now = new Date();
-        
-        diff = (now-DATE)/msInYear;
-        absDiff = Math.abs(diff);
+            diff = (now - DATE) / msInYear;
+            absDiff = Math.abs(diff);
 
-        return Math.round(absDiff) + ' years from now';
+            return Math.round(absDiff) + ' years from now';
+        }
+        else {
+            alert('Inputed date template is incorrect!');
+            resultFromNow.style.display = "none";
+        }
     };
-    
+
 
     return {
         format,
         fromNow
     }
-    
+
 })();
--- 3/3.1/js/main.js
+++ 3/3.1/js/main.js
@@ -2,34 +2,37 @@ var insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE;
 
 var submitBtn = document.getElementById("submitBtn");
 var fromNow = document.getElementById("fromNow");
+var resultDate = document.getElementById("resultDate");
+var resultFromNow = document.getElementById("resultFromNow");
+
 
 submitBtn.addEventListener("click", btnClick);
 fromNow.addEventListener("click", btnFromNow);
 
-function btnClick (e){
+function btnClick(e) {
     e.preventDefault();
-    insDate = document.forms["form"].elements["insDate"].value;
-    INPUT_TEMPLATE = document.forms["form"].elements["INPUT_TEMPLATE"].value;
-    OUTPUT_TEMPLATE = document.forms["form"].elements["OUTPUT_TEMPLATE"].value;
-        if ((INPUT_TEMPLATE.indexOf('D')!=-1) && (INPUT_TEMPLATE.indexOf('M')!=-1) && (INPUT_TEMPLATE.indexOf('Y')!=-1) &&
-            (OUTPUT_TEMPLATE.indexOf('D')!=-1) && ((OUTPUT_TEMPLATE.indexOf('M')!=-1) || (OUTPUT_TEMPLATE.indexOf('month')!=-1)) &&
-            (OUTPUT_TEMPLATE.indexOf('Y')!=-1)){
-            document.getElementById("resultDate").innerHTML = mod.format(insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE);
-            }
-        else {
-            alert('Inputed date template is incorrect!');
-        }
+    insDate = document.getElementById("insDate").value;
+    INPUT_TEMPLATE = document.getElementById("INPUT_TEMPLATE").value;
+    OUTPUT_TEMPLATE = document.getElementById("OUTPUT_TEMPLATE").value;
+    if ((INPUT_TEMPLATE.indexOf('D') != -1) && (INPUT_TEMPLATE.indexOf('M') != -1) && (INPUT_TEMPLATE.indexOf('Y') != -1) &&
+        (OUTPUT_TEMPLATE.indexOf('D') != -1) && ((OUTPUT_TEMPLATE.indexOf('M') != -1) || (OUTPUT_TEMPLATE.indexOf('month') != -1)) &&
+        (OUTPUT_TEMPLATE.indexOf('Y') != -1)) {
+        resultDate.innerHTML = mod.format(insDate, INPUT_TEMPLATE, OUTPUT_TEMPLATE);
+    }
+    else {
+        alert('Inputed date template is incorrect!');
+    }
 
 };
 
-function btnFromNow (e){
+function btnFromNow(e) {
     e.preventDefault();
-    insDate = document.forms["form"].elements["insDate"].value;
-    INPUT_TEMPLATE = document.forms["form"].elements["INPUT_TEMPLATE"].value;
-    if ((INPUT_TEMPLATE.indexOf('D')!=-1) && (INPUT_TEMPLATE.indexOf('M')!=-1) && (INPUT_TEMPLATE.indexOf('Y')!=-1)){
-            document.getElementById("resultFromNow").innerHTML = mod.fromNow(insDate, INPUT_TEMPLATE);
-            }
-        else {
-            alert('Inputed date template is incorrect!');
-        }
+    insDate = document.getElementById("insDate").value;
+    INPUT_TEMPLATE = document.getElementById("INPUT_TEMPLATE").value;
+    if ((INPUT_TEMPLATE.indexOf('D') != -1) && (INPUT_TEMPLATE.indexOf('M') != -1) && (INPUT_TEMPLATE.indexOf('Y') != -1)) {
+        resultFromNow.innerHTML = mod.fromNow(insDate, INPUT_TEMPLATE);
+    }
+    else {
+        alert('Inputed date template is incorrect!');
+    }
 };
