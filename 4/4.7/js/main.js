9235ab99c13aa513103f8e55c0d15cafd9e91dba
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 15:53:33 +0300

    4.7 task fixed

--- 4/4.7/js/main.js
+++ 4/4.7/js/main.js
@@ -1,15 +1,34 @@
 (function () {
     var arr = [1, 2, 3, 5, 0, 4, 0, 15, 12];
 
-    var even = arr.filter(function (curr) {
+    var myReduce = function (array, cb, initialVal) {
+        var val = initialVal;
+        for (var i = 0; i < array.length; i++) {
+            val = cb.call(null, val, array[i], i, array);
+        }
+        return val;
+    };
+
+    var myFilter = function (array, cb) {
+        outputArr = [];
+        for (var i = 0; i < array.length; i++) {
+            if (cb.call(null, array[i], i, array))
+                outputArr.push(array[i]);
+        }
+        return outputArr;
+    };
+
+    var even = myFilter(arr, function (curr) {
         return curr % 2 === 0;
     });
 
-    var avg = even.reduce(function (sum, curr) {
+    var arrSum = myReduce(even, function (sum, curr) {
         return sum += curr;
-    }) / even.length;
+    }, 0);
+    
+    var avg = arrSum / even.length;
 
-    console.log(arr);
-    console.log(even);
-    console.log(avg);
+    console.log('Array is: '+ arr);
+    console.log('Array of even numbers is '+ even);
+    console.log('The average on even numbers is: ' + avg);
 })();
