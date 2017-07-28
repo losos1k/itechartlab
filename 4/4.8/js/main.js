9d62e94703bd515bdb848abda9858cf3b07c4640
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 16:03:53 +0300

    4.8 task fixed

--- 4/4.8/js/main.js
+++ 4/4.8/js/main.js
@@ -5,10 +5,18 @@
     }
     var arr = fillArray(10);
 
-    var sum = arr.reduce(function (sum, curr){
+    var myReduce = function (array, cb, initialVal = 0) {
+        var val = initialVal;
+        for (var i = 0; i < array.length; i++) {
+            val = cb.call(null, val, array[i], i, array);
+        }
+        return val;
+    };
+
+    var sum = myReduce(arr, function (sum, curr){
         return sum += curr;
     });
 
-    console.log(arr);
-    console.log(sum);
+    console.log('Array is: ' + arr);
+    console.log('Array elements sum is: '+  sum);
 })();
