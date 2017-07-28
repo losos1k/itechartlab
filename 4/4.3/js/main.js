06247ae61eedafa8133385067be53426014ca204
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 14:30:40 +0300

    4.3 task fixed

--- 4/4.3/js/main.js
+++ 4/4.3/js/main.js
@@ -6,5 +6,20 @@
     }, 0)
 
     console.log(arr);
-    console.log(res);
+    console.log('Native reduce result: ' + res);
+
+
+    var myReduce = function (array, cb, initialVal) {
+        var val = initialVal;
+        for (var i = 0; i < array.length; i++) {
+            val = cb.call(null, val, array[i], i, array);
+        }
+        return val;
+    };
+
+    var res = myReduce(arr, function (sum, curr) {
+        return sum += curr.length;
+    }, 0);
+
+    console.log('My reduce result: ' + res);
 })();
