e5f416235b72980a2df0c9e11d4151fa14f4893b
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 15:40:20 +0300

    4.5 task fixed

--- 4/4.6/js/main.js
+++ 4/4.6/js/main.js
@@ -2,9 +2,25 @@
     var arr = [12, 5, undefined, 'aoa', 65, 1024, 'kek', 8];
 
     var res = arr.filter(function (curr) {
-        return typeof(curr) === 'number';
+        return typeof (curr) === 'number';
     });
 
     console.log(arr);
-    console.log(res);
+    console.log('Native filter result: ' + res);
+
+
+    var myFilter = function (array, cb) {
+        outputArr = [];
+        for (var i = 0; i < array.length; i++) {
+            if (cb.call(null, array[i], i, array))
+                outputArr.push(array[i]);
+        }
+        return outputArr;
+    };
+
+    var resMyFilter = myFilter(arr, function (curr) {
+        return typeof (curr) === 'number';
+    });
+
+    console.log('My filter result: ' + resMyFilter);
 })();
