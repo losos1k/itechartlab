2bbafee54b79e46f617ed60438ba814bffb50e19
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 16:12:44 +0300

    4.9 task fixed

--- 4/4.9/js/main.js
+++ 4/4.9/js/main.js
@@ -1,10 +1,19 @@
 (function () {
     var arr = ['v1', 'v', 'val1', 'vvv3', 'val57439', '123'];
 
-    var res = arr.filter(function (curr) {
+    var getFirst = function (array, cb) {
+        outputArr = [];
+        for (var i = 0; i < array.length; i++) {
+            if (cb.call(null, array[i], i, array))
+                outputArr.push(array[i]);
+        }
+        return outputArr[0];
+    };
+
+    var res = getFirst(arr, function (curr) {
         return curr.length > 2;
     });
 
-    console.log(arr);
-    console.log(res[0]);
+    console.log('Array is: ' + arr);
+    console.log('The first element that satisfies given condition: ' + res);
 })();
