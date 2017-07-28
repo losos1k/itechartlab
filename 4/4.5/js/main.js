22e7439529af2b7732f4f7e629cb458d66657f91
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-07-28 15:26:55 +0300

    4.5 task fixed

--- 4/4.5/js/main.js
+++ 4/4.5/js/main.js
@@ -4,5 +4,22 @@
     var res = arr.map(function (curr) {
         return curr.length;
     });
-    console.log(res);
+
+    console.log(arr);
+    console.log('Native map result: ' + res);
+
+
+    var myMap = function (array, cb) {
+        outputArr = [];
+        for (var i = 0; i < array.length; i++) {
+            outputArr.push(cb(array[i], i, array));
+        }
+        return outputArr;
+    };
+
+    var resMyMap = myMap(arr, function (curr) {
+        return curr.length;
+    });
+
+    console.log('My reduce result: ' + resMyMap);
 })();
