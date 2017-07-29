afe3d74200b25f8897dc30b7775446d42b8b6d6b
losos1k <eryka.mikhno@gmail.com>
2017-07-29 15:23:44 +0300

    4.4 task complete

--- 4/4.4/js/main.js
+++ 4/4.4/js/main.js
@@ -1,9 +1,34 @@
 (function () {
-    var arr = [12, 9];
+    function unfold(callback, initialValue) {
+        var res = [];
+        var num = initialValue;
+        var state = initialValue;
+        while (state) {
+            var arr = callback(state);
+            num = arr[0];
+            state = arr[1];
+            res.push(num);
+        }
+        return res;
+    }
 
-    
+    function decrement(number) {
+        var newElement = number--;
+        var newState = number--;
+        newState = newState > 0 ? newState : null;
+        return [newElement, newState];
+    }
+
+    function minDivider(number) {
+        if (number === 1) return [1, null];
+        for (var i = 2; i < Math.floor(number / 2); i++) {
+            if (number % i === 0) {
+                return [i, ~~(number / i)]
+            }
+        }
+        return [number, 1];
+    }
+
+    console.log(unfold(decrement, 12));
+    console.log(unfold(minDivider, 12));
 })();
-
-cb (val){
-    return 
-}
