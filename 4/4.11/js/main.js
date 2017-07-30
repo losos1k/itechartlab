6f89442e4ff61bbc88333be69dce44b4bddf73c1
losos1k <eryka.mikhno@gmail.com>
2017-07-30 12:13:14 +0300

    4.11 task complete

--- 4/4.11/js/main.js
+++ 4/4.11/js/main.js
@@ -1,3 +1,24 @@
 (function () {
-    
+    function lazyEvalMemo(fn) {
+        var args = arguments;
+        var res;
+        var lazyEval = fn.bind.apply(fn, args);
+        return function () {
+            if (res) {
+                return 'the result was already calculated: ' + res;
+            }
+            res = lazyEval();
+            return 'calculated result is: ' + res;
+        }
+    }
+
+    function factorial(num) {
+        return (num != 1) ? num * factorial(num - 1) : 1;
+    }
+
+
+    var lazyMemoSum = lazyEvalMemo(factorial, 3);
+
+    console.log(lazyMemoSum());
+    console.log(lazyMemoSum());
 })();
