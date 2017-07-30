8a7e6bf60f3b3638daad1b87cd080bbe5e30de77
losos1k <eryka.mikhno@gmail.com>
2017-07-30 22:47:42 +0300

    4.10 task complete

--- 4/4.10/js/main.js
+++ 4/4.10/js/main.js
@@ -1,13 +1,14 @@
 (function () {
-    function lazyEval(fn) {
-        return fn.bind.apply(fn, arguments);
+    function lazyEval(func) {
+        console.log('the result has not been calculated yet');        
+        return func.bind.apply(func, arguments);
     }
 
     function add(a, b) {
-        return a + b;
+        return 'the result is calculating here: ' + (a + b);
     }
 
-    var kek = lazyEval(add, 2, 3);
+    var calc = lazyEval(add, 2, 3);
 
-    console.log(kek());
+    console.log(calc());
 })();
