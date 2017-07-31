9fba9fd4aca6f3d08b85588a16b3a88cc039d2e0
losos1k <eryka.mikhno@gmail.com>
2017-07-31 20:23:47 +0300

    4.2 task fixed

--- 4/4.2/js/main.js
+++ 4/4.2/js/main.js
@@ -5,28 +5,37 @@
     function curr(func) {
         var args = [];
         var res;
+        var resArr = [];
         return function innerCurr(...args) {
             for (var i = 0; i < args.length; i++) {
                 if (args.length > 1) {
                     innerCurr(args[i]);
                 }
-                res = func.call(null, args[i]);
+                var res = func.call(null, args[i]);
             }
-            console.log(arguments);
-            console.log(res);
+            if (args.length <= 1) {
+                console.log('arguments of this function: ' + args);
+                resArr.push(res);
+            }
 
-            return res;
+            return 'the answer is: ' + resArr.reduce(function (sum, curr) {
+                return sum += curr;
+            });
         };
     };
 
     function squareSum() {
-        var resArr = [];
-        var kek = Array.prototype.slice.call(arguments).reduce(function (sum, curr) {
+        return Array.prototype.slice.call(arguments).reduce(function (sum, curr) {
             return sum += Math.pow(curr, 2);
         }, 0);
-        // resArr.push(kek);
-        return kek;
+    }
+
+    function sumsum() {
+        return Array.prototype.slice.call(arguments).reduce(function (sum, curr) {
+            return sum += curr;
+        }, 0);
     }
 
     console.log(curr(squareSum)(2, 3, 5));
+    console.log(curr(sumsum)(1, 2, 3, 4, 1));
 })();
