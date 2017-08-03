de0c5195e415884f7be49aabd9e2afa4888372d8
Eryka-Ragna Mikhno <eryka.mikhno@gmail.com>
2017-08-03 20:05:57 +0300

    refactor

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,25 +1,28 @@
 (function ($) {
-    $('.drop-area').dragAndDrop();
+    $(document).ready(function () {
 
-    $('#submitForm').click(function () {
-        function checkForm() {
-            $('input').each(function () {
-                if ($('input').val().length > 0) {
-                    return true;
-                } else {
-                    return false;
-                }
+        $('.drop-area').dragAndDrop();
+
+        $('#submitForm').click(function () {
+            function checkForm() {
+                $('input').each(function () {
+                    if ($('input').val().length > 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+            };
+
+            var promise = new Promise((resolve, reject) => {
+                checkForm('#input-text') ? resolve(result) : reject();
             });
-        };
 
-        var promise = new Promise((resolve, reject) => {
-            checkForm('#input-text') ? resolve(result) : reject();
+            promise
+                .then(
+                result => console.log('Fulfilled: '),
+                error => console.log('Rejected')
+                );
         });
-
-        promise
-            .then(
-            result => console.log('Fulfilled: '),
-            error => console.log('Rejected')
-            );
     });
 })(jQuery);
