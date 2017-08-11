0fc82ea6e52d262ca5cc6c3b01c2a432542a04f4
losos1k <eryka.mikhno@gmail.com>
2017-08-11 17:33:41 +0300

    webpack configured

--- 6/webpack.config.js
+++ 6/webpack.config.js
@@ -1,23 +1,29 @@
-var webpack = require('webpack');
-var path = require('path');
+var path = require("path");
 
-module.exports = {
-    entry: './script.js',
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/index.js",
     output: {
-        path: __dirname + "/js",
-        filename: "scripts.min.js"
+        path: DIST_DIR + "/js",
+        filename: "bundle.js",
+        publicPath: "/js/"
     },
     module: {
         loaders: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
                 query: {
-                    presets: ['es2015', 'react'],
-                    plugins: ['transform-class-properties'],
+                    presets: ["react", "es2015", "stage-2"],
+                    plugins: ["transform-decorators-legacy"]
                 }
-            }
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
-    },
-}
+    }
+};
+
+module.exports = config;
