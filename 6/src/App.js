523a82587116b8732dd16637939cf4b252f8ba2d
losos1k <eryka.mikhno@gmail.com>
2017-08-24 23:49:07 +0300

    getMovies action connected to Login component instead of App

--- 6/src/App.js
+++ 6/src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux';
 import storeCreate from './storeCreate'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import { getMoviesAction } from './MovieListPage/getMoviesAction';
 
 import Login from './LoginPage/index'
 import MoviesList from './MovieListPage/index'
 import MovieInfo from './MoviePage/index'
 
-const mapStateToProps = (store) => {
-  return {
-    movies: store.movies.movies
-  };
-}
-
-const mapDispatchToProps = () => {
-  return dispatch => ({
-    getMoviesAction: () => dispatch(getMoviesAction())
-  })
-}
-
-@connect(mapStateToProps, mapDispatchToProps)
 class App extends Component {
   constructor() {
     super();
@@ -30,21 +14,17 @@ class App extends Component {
     this.store = storeCreate();
   }
 
-  componentWillMount () {
-    this.props.getMoviesAction();
-  }
-
   render() {
     return (
       <BrowserRouter history={history}>
-      <MuiThemeProvider>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route path="/movies" component={MoviesList} />
-            <Route path="/movie/:id" component={MovieInfo} />
-          </Switch>
-        </div>
+        <MuiThemeProvider>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route path="/movies" component={MoviesList} />
+              <Route path="/movie/:id" component={MovieInfo} />
+            </Switch>
+          </div>
         </MuiThemeProvider>
       </BrowserRouter>
     );
