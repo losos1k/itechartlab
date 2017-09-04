363f5d30b88d7467fc79ba7074d3cebd0d6de490
losos1k <eryka.mikhno@gmail.com>
2017-09-04 21:30:02 +0300

    users fixed

--- 6/backend/data/users.js
+++ 6/backend/data/users.js
@@ -1,28 +1,21 @@
 import express from 'express';
-import passport from 'passport';
 import dbUsersModel from '../models/users';
 import bcrypt from 'bcryptjs';
-
-var LocalStrategy = require('passport-local').Strategy;
 
 export let createUser = (user, callback) => {
   var newUser = new dbUsersModel(user);
 
-  dbUsersModel.createUser = (newUser, callback) => {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        newUser.password = hash;
-        newUser.save(callback);
-      });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      newUser.password = hash;
+      newUser.save(callback);
     });
-  };
-
-  dbUsersModel.createUser(newUser, callback);
-}
+  });
+};
 
 export let getUserByName = (login, callback) => {
   return dbUsersModel.findOne({ login: login }, callback)
-}
+};
 
 export let comparePassword = (candidatePassword, hash, callback) => {
   bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
@@ -32,4 +25,4 @@ export let comparePassword = (candidatePassword, hash, callback) => {
 
     callback(null, isMatch);
   });
-}
+};
